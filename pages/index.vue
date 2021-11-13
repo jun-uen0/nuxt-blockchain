38b7ec16259cd8d782ef8c2c3096e4ef2e3bef2c
JunJun <hello@jaysan.io>
2021-11-13 23:07:37 +0900

    1st commit

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<p>
+			<input
+			type="text"
+			v-model="inputNumber"
+			placeholder="input number"
+			>
+			<button
+			@click="setNumber()"
+			>
+			Set Number to contract
+			</button>
+		</p>
+		<p>
+			<button
+			@click="getNumber()"
+			>
+			Get Number from contract
+			</button>
+			Number:{{number}}
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 0, // retrive from contract
+      inputNumber: 0 // input form
+    }
+  },
+  methods: {
+    getNumber: async function() {
+      let ret = await this.$contract.methods.get().call()
+      console.log(ret)
+      console.log(this.$web3.eth.getAccounts)
+      this.number = ret
+    },
+    setNumber: async function() {
+      let accounts = await this.$web3.eth.getAccounts()
+      let account = accounts[0]
+      let ret = await this.$contract.methods.set(this.inputNumber).send({from: account})
+    },
+  },
+  mounted() {
+    console.log('Current Block Number')
+    this.$web3.eth.getBlockNumber().then(console.log)
+  }
+}
+</script>
