ebda92b4f10e9ebf01b7f435e4c62a930c78623b
jun-uen0 <hi@jaysan.io>
2021-11-30 01:17:58 +0900

    clean up

--- README.md
+++
@@ -1,5 +0,0 @@
-# nuxt-web3
-Smartcontract app using Nuxt.js<br>
-https://c2d90430.nuxt-blockchain.pages.dev/
-
-- Hosting : Cloudflare
--- migrations/2_deploy_contract.js
+++
@@ -1,5 +0,0 @@
-const SingleNumRegister = artifacts.require('SingleNumRegister')
-
-module.exports = function (deployer) {
-	deployer.deploy(SingleNumRegister)
-}
